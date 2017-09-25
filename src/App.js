cdd5ed5285828c4f55f3ccdaceb94ad3594725db
전영자 <jasmine.g@navercorp.com>
2017-09-25 11:50:57 +0900

    고스트 - draggable 가능 유무에 따라 분기처리

--- src/App.js
+++ src/App.js
@@ -14,19 +14,29 @@ class App extends Component {
   }
 
   render() {
+
+    const draggable = this._isDraggable();
+
     return (
       <div className="App"
         ref={el=> {this._app = el;}}
-        onMouseDown={this._mousedown}
-        onMouseUp={this._mouseup}>
+        onClick={ this.handleClick }
+        onMouseDown={this.handleMouseDown}
+        onMouseUp={this.handleMouseUp}
+        onDragStart={ this.handleDragStart }
+        onDragOver={ this.handleDragOver }
+        onDragEnd={ this.handleDragEnd }
+        >
         <div className="App-header">
           <div id="drag" className="drag"></div>
           <img src="https://images.unsplash.com/photo-1428550443830-190057dc8098?dpr=1&auto=compress,format&fit=crop&w=767&h=511&q=80&cs=tinysrgb&crop="
             alt="unsplash-1"
+            draggable={ draggable }
             width="458"
             height="305"/>
           <img src="https://images.unsplash.com/photo-1475759697283-38cc56e4455a?dpr=1&auto=compress,format&fit=crop&w=767&h=511&q=80&cs=tinysrgb&crop="
             alt="unsplash-2"
+            draggable={ draggable }
             width="458"
             height="305"/>
         </div>
@@ -34,37 +44,82 @@ class App extends Component {
     );
   }
 
+  _isDraggable() {
+      const isDraggable = 'function' === typeof DataTransfer.prototype.setDragImage;
+
+      // console.log("isDraggable ", isDraggable);
+
+      return isDraggable;
+  }
+
   _defineMouseEvent() {
-    this._mousedown = this._mousedown.bind(this);
-    this._mousemove = this._mousemove.bind(this);
-    this._mouseup = this._mouseup.bind(this);
+      this.handleClick = this.handleClick.bind(this);
+      this.handleMouseDown = this.handleMouseDown.bind(this);
+      this.handleMouseMove = this.handleMouseMove.bind(this);
+      this.handleMouseUp = this.handleMouseUp.bind(this);
+      this.handleDragStart = this.handleDragStart.bind(this);
+      this.handleDragOver = this.handleDragOver.bind(this);
+      this.handleDragEnd = this.handleDragEnd.bind(this);
   }
 
-  _mousedown(e) {
-    document.body.addEventListener("mousemove", this._mousemove);
-    e.preventDefault();
-
-    const drag = document.getElementById("drag");
-
-    const target = e.target;
-    this._img.src = target.src;
-    this._img.width = Math.round(target.width*(25/100));
-    this._img.height = Math.round(target.height*(25/100));
-
-    drag.appendChild(this._img);
+  handleClick(e) {
+    
   }
 
-  _mousemove(e) {
-    const drag = document.getElementById("drag");
-    drag.style.top = Math.round(e.pageY) + "px";
-    drag.style.left = Math.round(e.pageX) + "px";
+  handleMouseDown(e) {
+      if (this._isDraggable()) {
+          return;
+      }
+
+      document.body.addEventListener("mousemove", this.handleMouseMove);
+      e.preventDefault();
+
+      const drag = document.getElementById("drag");
+
+      const target = e.target;
+      this._img.src = target.src;
+      this._img.width = Math.round(target.width*(25/100));
+      this._img.height = Math.round(target.height*(25/100));
+
+      drag.appendChild(this._img);
   }
 
-  _mouseup(e) {
-    document.body.removeEventListener("mousemove", this._mousemove);
+  handleMouseMove(e) {
+      const drag = document.getElementById("drag");
+      drag.style.top = Math.round(e.pageY) + "px";
+      drag.style.left = Math.round(e.pageX) + "px";
+  }
 
-    const drag = document.getElementById("drag");
-    drag.innerHTML = "";
+  handleMouseUp(e) {
+      if (this._isDraggable()) {
+          return;
+      }
+
+      document.body.removeEventListener("mousemove", this.handleMouseMove);
+
+      const drag = document.getElementById("drag");
+      drag.innerHTML = "";
+  }
+
+  handleDragStart(e) {
+      const drag = document.getElementById("drag");
+
+      const target = e.target;
+      this._img.src = target.src;
+      this._img.width = Math.round(target.width*(25/100));
+      this._img.height = Math.round(target.height*(25/100));
+
+      drag.appendChild(this._img);
+
+      e.dataTransfer.setDragImage(drag, 5, 5);
+  }
+
+  handleDragOver(e) {
+    
+  }
+
+  handleDragEnd(e) {
+    
   }
 }
 
